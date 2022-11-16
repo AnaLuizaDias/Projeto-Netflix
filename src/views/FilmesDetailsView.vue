8b6dbf789364bd602650fd1760fcdee3bf4b3bb6
Ana <analuizadiasdarocha212@gmail.com>
2022-11-16 11:13:09 -0300

    Busca dos detalhes de filmes concluída

--- src/views/FilmesDetailsView.vue
+++ src/views/FilmesDetailsView.vue
@@ -4,13 +4,20 @@ export default {
   props: ["id"],
   data() {
     return {
-      details: [],
+      details: {},
     };
   },
   methods: {
     getImageUrl(poster_path) {
       return `https://image.tmdb.org/t/p/w500/${poster_path}`;
     },
+  },
+  async created() {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${this.id}?api_key=ed777039147c6c57657810892e0b2acd&language=pt-BR`
+    );
+    // this.filmes = res.data.results;
+    this.details = data;
   },
 };
 </script>
@@ -23,29 +30,29 @@ export default {
         <div class="col-10 col-sm-8 col-lg-6">
           <div
             class="col mt-4"
-            v-for="filme of filmes"
-            :key="filme.id"
-            @click="go(filme.id)"
-          >
-            <img :src="getImageUrl(filme.poster_path)" />
-          </div>
+            v-for="detail of details"
+            :key="detail.id"
+          ></div>
         </div>
-        <div class=" texto">
-          <h1 class="display-5 fw-bold lh-1 mb-3">
-            Responsive left-aligned hero with image
-          </h1>
-          <p class="lead">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
-          </p>
-          
-          <div class="img col-lg-4">
-            <img src="https://s2.glbimg.com/0g5TjC-0T1ZEkGG6Vh697NKwTnA=/0x0:1200x700/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/o/D/ieDFWBTXyor9nFgYZdyA/enola-holmes-2.jpg" alt="">
-          </div>
+      </div>
+      <div class="texto">
+        <h1 class="display-5 fw-bold lh-1 mb-3">
+          Responsive left-aligned hero with image
+        </h1>
+        <p class="lead">
+          Quickly design and customize responsive mobile-first sites with
+          Bootstrap, the world’s most popular front-end open source toolkit,
+          featuring Sass variables and mixins, responsive grid system, extensive
+          prebuilt components, and powerful JavaScript plugins.
+        </p>
+        {{ details }}
+
+        <div class="img col-lg-4">
+          <img
+            src="https://s2.glbimg.com/0g5TjC-0T1ZEkGG6Vh697NKwTnA=/0x0:1200x700/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/o/D/ieDFWBTXyor9nFgYZdyA/enola-holmes-2.jpg"
+            alt=""
+          />
         </div>
-        
       </div>
     </div>
   </main>
@@ -64,13 +71,11 @@ main {
 template {
   background-color: black;
 }
-.img{
+.img {
   display: flex;
   width: 30%;
-  
 }
-.texto{
+.texto {
   display: flex;
-  
 }
 </style>
